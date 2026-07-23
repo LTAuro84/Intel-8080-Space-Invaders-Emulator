85bf46c546ab5084f52b02396a59505efb57cf5f
LTAuro84 <sarm20@icloud.com>
2026-07-23 16:28:06 -0400

    writing instructions

--- cpu.c
+++ cpu.c
@@ -7,4 +7,24 @@ void UnimplementedInstruction(State8080 *state) {
 	exit(1);
 }
 
-int Emulate8080Op(State8080 *state)
+int Emulate8080Op(State8080 *state) {
+	
+	unsigned char *opcode = &state->memory[state->pc];
+
+	switch(*opcode) {
+	case 0x00: break; //NOP
+	case 0x01:
+		state->c = opcode[1];
+		state->b = opcode[2];
+		state->pc += 2;
+		break;
+	case 0x06:
+		state->b = opcode[1];
+		state->pc++;
+		break;
+	case 0x0e:
+		state->c = opcode[1];
+		state->pc++;
+		break;
+	}
+}
